9c3845462c33519fa47a7adb2e7cdadf6cc82622
Sameer Saharan <114926999+sameer-saharan@users.noreply.github.com>
2025-02-03 10:55:41 +0530

    NEW: asyncHandler util

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,20 @@
+export const asyncHandler = (requestHandler) => {
+    (req, res, next) => {
+        Promise
+        .resovle(requestHandler(req, res, next))
+        .catch((err) => next(err));
+    }
+};
+
+/*
+export const asyncHandler = (fn) => async (req, res, next) => {
+    try {
+        await fn(req, res, next)
+    } catch (err) {
+        res.status(err.code || 500).json({
+            success: false,
+            message: err.message
+        });
+    }
+};
+*/
