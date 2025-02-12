4cf7ad6ce6857068c646793e8b06da7a9d82d626
Sameer Saharan <114926999+saharansameer@users.noreply.github.com>
2025-02-12 12:11:23 +0530

    REFACTOR: converted Promise resolve/reject syntax to async/await

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,20 +1,21 @@
 export const asyncHandler = (requestHandler) => {
+    return async (req, res, next) => {
+        try {
+            await requestHandler(req, res, next);
+        } catch (err) {
+            next(err);
+        }
+    };
+};
+
+
+// Resolve/Reject Syntax
+/* 
+export const asyncHandler = (requestHandler) => {
     return (req, res, next) => {
         Promise
         .resolve(requestHandler(req, res, next))
         .catch((err) => next(err));
     }
-};
-
-/*
-export const asyncHandler = (fn) => async (req, res, next) => {
-    try {
-        await fn(req, res, next)
-    } catch (err) {
-        res.status(err.code || 500).json({
-            success: false,
-            message: err.message
-        });
-    }
-};
+}
 */
