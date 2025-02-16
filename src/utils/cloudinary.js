ab3fe587a00e297b6db032a80d81a4efa3213c7a
Sameer Saharan <114926999+saharansameer@users.noreply.github.com>
2025-02-16 21:56:52 +0530

    fix: added await with uploader  and remove files from public/temp dir after upload on cloud

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -1,19 +1,21 @@
 import { v2 as cloudinary } from "cloudinary";
-import { response } from "express";
 import fs from "fs";
 
 cloudinary.config({
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
     api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: process.env.CLOUDINARY_API_SECRET,
+    api_secret: process.env.CLOUDINARY_API_SECRET
 });
 
 const uploadOnCloudinary = async (localFilePath) => {
     try {
         if (!localFilePath) return null;
-        cloudinary.uploader.upload(localFilePath, {
-            resource_type: "auto",
+        const response = await cloudinary.uploader.upload(localFilePath, {
+            resource_type: "auto"
         });
+        // Removes files from public/temp directory after uploading to cloudinary
+        fs.unlinkSync(localFilePath);
+        
         return response;
     } catch (err) {
         fs.unlinkSync(localFilePath);
