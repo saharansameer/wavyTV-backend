57bdb10841bf4bdbe45a5cfa1c2ac7e727e1d033
Sameer Saharan <114926999+saharansameer@users.noreply.github.com>
2025-02-16 21:54:22 +0530

    feat: avatar and coverImage upload on cloudinary

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,16 +1,9 @@
 import ApiResponse from "../utils/apiResponse.js";
 import ApiError from "../utils/apiError.js";
 import { User } from "../models/user.model.js";
+import { uploadOnCloudinary } from "../utils/cloudinary.js";
 
 const registerUser = async (req, res) => {
-    // get user details from FE
-    // validation - not empty
-    // check is user already exists - username, email
-    // check for image and avatar, upload them on cloudinary
-    // create user object and create entry in db
-    // remove password and refresh token field from response
-    // check for user creation
-    // return res
     const { fullName, username, email, password } = req.body;
 
     // checks if any field is empty or not
@@ -30,34 +23,74 @@ const registerUser = async (req, res) => {
     }
 
     // checks if user already exists or not
-    const userExists = await User.findOne({ $or: [{username},{email}]});
+    const userExists = await User.findOne({ $or: [{ username }, { email }] });
     if (userExists) {
         // checks if username already exists
         if (userExists.username === username) {
-            throw new ApiError({status: 409, message: "username already exists"});
+            throw new ApiError({
+                status: 409,
+                message: "username already exists"
+            });
         }
         // checks if email already exists
         if (userExists.email === email) {
-            throw new ApiError({status: 409, message: "Email already exists"});
+            throw new ApiError({
+                status: 409,
+                message: "Email already exists"
+            });
         }
     }
 
     // password validation (not active yet)
     // const passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/gm
 
-    // user creation
-    const createdUser = await User.create({
-        fullName, username, email, password
-    });
-    // checks if user document created successfully
-    if (!createdUser) {
-        throw new ApiError({status: 400, message: "Error while creating user"});
+    // avatar and cover image local path config
+    const avatarLocalPath = req.files?.avatar[0]?.path;
+    const coverImageLocalPath = req.files?.coverImage[0]?.path;
+    if (!avatarLocalPath) {
+        throw new ApiError({ status: 400, message: "Avatar is required" });
     }
 
-    return res.json(new ApiResponse({status: 201, message: "User registered successfully!", data: {username, email}}));
+    // uploading avatar on cloud
+    const avatar = await uploadOnCloudinary(avatarLocalPath);
+    const coverImage = await uploadOnCloudinary(coverImageLocalPath);
+    if (!avatar) {
+        throw new ApiError({ status: 400, message: "Unable to upload avatar" });
+    }
+    if (!coverImage) {
+        throw new ApiError({
+            status: 400,
+            message: "Unable to upload cover image"
+        });
+    }
 
+    // user creation
+    const createdUser = await User.create({
+        fullName,
+        username,
+        email,
+        password,
+        avatar: avatar.url,
+        coverImage: coverImage?.url || ""
+    });
 
+    // clears password from body
 
+    // checks if user document created successfully
+    if (!createdUser) {
+        throw new ApiError({
+            status: 400,
+            message: "Error while creating user"
+        });
+    }
+
+    return res.json(
+        new ApiResponse({
+            status: 201,
+            message: "User registered successfully!",
+            data: { username, email }
+        })
+    );
 };
 
 export { registerUser };
