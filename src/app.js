8e49021dc7056aa7b48a6ce14efb8957321c9ce7
Sameer Saharan <114926999+saharansameer@users.noreply.github.com>
2025-02-17 22:27:31 +0530

    fix: urlencoded extened: true

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ app.use(cors({
 }));
 
 app.use(express.json({limit: SIZE_LIMIT})); 
-app.use(express.urlencoded({extended: false, limit: SIZE_LIMIT}));
+app.use(express.urlencoded({extended: true, limit: SIZE_LIMIT}));
 app.use(express.static("public"));
 app.use(cookieParser());
 
