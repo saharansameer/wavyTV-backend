2ddec32e3be19d8525d0b44c7fbb4effc931a383
Sameer Saharan <114926999+saharansameer@users.noreply.github.com>
2025-02-17 22:27:09 +0530

    fix: error status undefined

--- src/middlewares/error.middleware.js
+++ src/middlewares/error.middleware.js
@@ -1,5 +1,5 @@
 const errorHandler = (err, req, res, next) => {
-    return res.status(err.status).json({
+    return res.status(err.status || 500).json({
         status: err.status,
         message: err.message,
         errors: err.errors || []
