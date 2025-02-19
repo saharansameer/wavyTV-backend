700c587247d777ce63716bf32440bb61e25af052
Sameer Saharan <114926999+saharansameer@users.noreply.github.com>
2025-02-19 23:11:02 +0530

    new: user routes configuration

---
+++ src/routes/user.routes.js
@@ -0,0 +1,42 @@
+import { Router } from "express";
+import { asyncHandler } from "../utils/asyncHandler.js";
+import { errorHandler } from "../middlewares/error.middleware.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
+import {
+    getCurrentUser,
+    updateUserAccountDetails,
+    updateUserChannelDetails,
+    changeUserPassword
+} from "../controllers/user.controller.js";
+import { upload } from "../middlewares/multer.middleware.js";
+
+const router = Router();
+
+// GET - current user details
+router
+    .route("/:username")
+    .get(asyncHandler(verifyJWT), asyncHandler(getCurrentUser));
+
+// PATCH - fullname, username, email
+router
+    .route("/:username")
+    .patch(asyncHandler(verifyJWT), asyncHandler(updateUserAccountDetails));
+
+// PATCH - avatar, coverImage
+router.route("/:username/channel").patch(
+    asyncHandler(verifyJWT),
+    upload.fields([
+        { name: "avatar", maxCount: 1 },
+        { name: "coverImage", maxCount: 1 }
+    ]),
+    asyncHandler(updateUserChannelDetails)
+);
+
+// PATCH - password
+router
+    .route("/:username/security")
+    .patch(asyncHandler(verifyJWT), asyncHandler(changeUserPassword));
+
+router.use(errorHandler);
+
+export default router;
