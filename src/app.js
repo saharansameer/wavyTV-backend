b2dc1123f534f0b658059ee4fe3ae49d5411a7ab
Sameer Saharan <114926999+saharansameer@users.noreply.github.com>
2025-02-19 20:56:09 +0530

    add: user router imported and configured

--- src/app.js
+++ src/app.js
@@ -5,20 +5,24 @@ import { SIZE_LIMIT } from "./constants.js";
 
 const app = express();
 
-app.use(cors({
-    origin: process.env.CORS_ORIGIN,
-    credentials: true
-}));
+app.use(
+    cors({
+        origin: process.env.CORS_ORIGIN,
+        credentials: true
+    })
+);
 
-app.use(express.json({limit: SIZE_LIMIT})); 
-app.use(express.urlencoded({extended: true, limit: SIZE_LIMIT}));
+app.use(express.json({ limit: SIZE_LIMIT }));
+app.use(express.urlencoded({ extended: true, limit: SIZE_LIMIT }));
 app.use(express.static("public"));
 app.use(cookieParser());
 
 // routes import
 import authRouter from "./routes/auth.routes.js";
+import userRouter from "./routes/user.routes.js";
 
 // routes declaration
 app.use("/auth", authRouter);
+app.use("/user", userRouter);
 
 export default app;
