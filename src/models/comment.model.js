e4adeb2cfa3d48d92ac2a093b27e97b796376d0b
Sameer Saharan <114926999+saharansameer@users.noreply.github.com>
2025-03-03 14:51:03 +0530

    new: comments model

---
+++ src/models/comment.model.js
@@ -0,0 +1,28 @@
+import mongoose, { Schema } from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+
+const commentSchema = new Schema(
+  {
+    content: {
+      type: String,
+      required: true
+    },
+    commentBy: {
+      type: Schema.Types.ObjectId,
+      ref: "User"
+    },
+    video: {
+      type: Schema.Types.ObjectId,
+      ref: "Video"
+    },
+    tweet: {
+      type: Schema.Types.ObjectId,
+      ref: "Tweet"
+    }
+  },
+  { timestamps: true }
+);
+
+commentSchema.plugin(mongooseAggregatePaginate);
+
+export const Comment = mongoose.model("Comment", commentSchema);
