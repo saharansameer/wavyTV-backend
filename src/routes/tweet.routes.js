a3b65d6c17dece164dc8a6df5d8a635aebea0b5e
Sameer Saharan <114926999+saharansameer@users.noreply.github.com>
2025-03-08 14:00:59 +0530

    add: added routes for getUserTweets method

--- src/routes/tweet.routes.js
+++ src/routes/tweet.routes.js
@@ -2,12 +2,15 @@ import { Router } from "express";
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { errorHandler } from "../middlewares/error.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
-import { createTweet } from "../controllers/tweet.controller.js";
+import { createTweet, getUserTweets } from "../controllers/tweet.controller.js";
 
 const router = Router();
 
 // POST - create new tweet
 router.route("/").post(asyncHandler(verifyJWT), asyncHandler(createTweet));
+
+// GET - fetch tweets by username
+router.route("/:username").get(asyncHandler(getUserTweets));
 
 // Error Handler
 router.use(errorHandler);
