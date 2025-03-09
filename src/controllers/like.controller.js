81046132836f1c2e2db505243afb048c10a12234
Sameer Saharan <114926999+saharansameer@users.noreply.github.com>
2025-03-09 20:14:35 +0530

    perf: optimize toggleVideoLike to reduce DB calls

--- src/controllers/like.controller.js
+++ src/controllers/like.controller.js
@@ -12,39 +12,34 @@ const toggleVideoLike = async (req, res) => {
     throw new ApiError({ status: 404, message: "Video does not exist" });
   }
 
-  // Fetch like document
-  const like = await Like.findOne({ video: videoId, likedBy: req.user._id });
-  // Video exists but not liked by user, so like it
-  if (!like) {
-    const likedVideo = await Like.create({
-      likedBy: req.user._id,
-      video: videoId
-    });
-    if (!likedVideo) {
-      throw new ApiError({ status: 400, message: "Unable to like video" });
-    }
+  // Videos exists and liked by user, so unlike it
+  const unlike = await Like.findOneAndDelete({
+    video: videoId,
+    likedBy: req.user._id
+  });
 
-    // Response: Video Liked
+  // Response: Video Unliked
+  if (unlike) {
     return res.status(200).json(
       new ApiResponse({
         status: 200,
-        message: "Video Liked successfully"
+        message: "Video Unliked successfully"
       })
     );
   }
 
-  // Videos exists and liked by user, so unlike it
-  try {
-    await Like.findByIdAndDelete(like._id);
-  } catch (err) {
-    throw new ApiError({ status: 500, message: err.message });
-  }
+  // Video exists but not liked by user, so like it
+  const like = await Like.create({
+    likedBy: req.user._id,
+    video: videoId
+  });
 
-  // Response: Video Unliked
+  // Response: Video Liked
   return res.status(200).json(
     new ApiResponse({
       status: 200,
-      message: "Video Unliked successfully"
+      message: "Video Liked successfully",
+      data: like
     })
   );
 };
