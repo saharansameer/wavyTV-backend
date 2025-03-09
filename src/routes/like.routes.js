f8420f610ac91a6ac3ee07c713e84f532cfd18fb
Sameer Saharan <114926999+saharansameer@users.noreply.github.com>
2025-03-09 20:05:38 +0530

    add: added routes for toggleVideoLike controller

---
+++ src/routes/like.routes.js
@@ -0,0 +1,17 @@
+import { Router } from "express";
+import { asyncHandler } from "../utils/asyncHandler.js";
+import { errorHandler } from "../middlewares/error.middleware.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
+import { toggleVideoLike } from "../controllers/like.controller.js";
+
+const router = Router();
+
+// POST - Toggle video like
+router
+  .route("/:videoId")
+  .post(asyncHandler(verifyJWT), asyncHandler(toggleVideoLike));
+
+// Error Handler
+router.use(errorHandler);
+
+export default router;
