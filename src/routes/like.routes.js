3f07cb1566b58c5af2f5af6724882b7443aa5629
Sameer Saharan <114926999+saharansameer@users.noreply.github.com>
2025-03-09 20:30:25 +0530

    add: added routes for toggleTweetLike controller

--- src/routes/like.routes.js
+++ src/routes/like.routes.js
@@ -2,7 +2,10 @@ import { Router } from "express";
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { errorHandler } from "../middlewares/error.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
-import { toggleVideoLike } from "../controllers/like.controller.js";
+import {
+  toggleVideoLike,
+  toggleTweetLike
+} from "../controllers/like.controller.js";
 
 const router = Router();
 
@@ -10,6 +13,11 @@ const router = Router();
 router
   .route("/video/:videoId")
   .post(asyncHandler(verifyJWT), asyncHandler(toggleVideoLike));
+
+// POST - Toggle tweet like
+router
+  .route("/tweet/:tweetId")
+  .post(asyncHandler(verifyJWT), asyncHandler(toggleTweetLike));
 
 // Error Handler
 router.use(errorHandler);
