ecb799739253842456b97de120f6e9ca71c88817
Sameer Saharan <114926999+saharansameer@users.noreply.github.com>
2025-03-09 20:27:13 +0530

    feat: added feature to toggle like of tweets

--- src/controllers/like.controller.js
+++ src/controllers/like.controller.js
@@ -2,6 +2,7 @@ import ApiError from "../utils/apiResponse.js";
 import ApiResponse from "../utils/apiResponse.js";
 import { Like } from "../models/like.model.js";
 import { Video } from "../models/video.model.js";
+import { Tweet } from "../models/tweet.model.js";
 
 const toggleVideoLike = async (req, res) => {
   const { videoId } = req.params;
@@ -44,4 +45,40 @@ const toggleVideoLike = async (req, res) => {
   );
 };
 
-export { toggleVideoLike };
+const toggleTweetLike = async (req, res) => {
+  const { tweetId } = req.params;
+
+  // Fetch Tweet document and check if Tweet exists
+  const tweet = await Tweet.findById(tweetId);
+  if (!tweet) {
+    throw new ApiError({ status: 404, message: "Tweet does not exist" });
+  }
+
+  // Tweet exists and liked by user, so unlike it
+  const unlike = await Like.findOneAndDelete({
+    tweet: tweetId,
+    likedBy: req.user._id
+  });
+
+  // Response: Tweet Unliked
+  if (unlike) {
+    return res
+      .status(200)
+      .json(
+        new ApiResponse({ status: 200, message: "Tweet unliked successfully" })
+      );
+  }
+
+  // Tweet exists but not liked by user, so like it
+  const like = await Like.create({
+    likedBy: req.user._id,
+    tweet: tweetId
+  });
+
+  // Response: Tweet Liked
+  return res
+    .status(200)
+    .json({ status: 200, message: "Tweet liked successfully", data: like });
+};
+
+export { toggleVideoLike, toggleTweetLike };
