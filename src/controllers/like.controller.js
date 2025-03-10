5cea848380dba059463711a3a3ed78597d3111bf
Sameer Saharan <114926999+saharansameer@users.noreply.github.com>
2025-03-10 22:37:43 +0530

    feat: added feature to toggle like on comment

--- src/controllers/like.controller.js
+++ src/controllers/like.controller.js
@@ -3,6 +3,7 @@ import ApiResponse from "../utils/apiResponse.js";
 import { Like } from "../models/like.model.js";
 import { Video } from "../models/video.model.js";
 import { Tweet } from "../models/tweet.model.js";
+import { Comment } from "../models/comment.model.js";
 
 const toggleVideoLike = async (req, res) => {
   const { videoId } = req.params;
@@ -81,4 +82,47 @@ const toggleTweetLike = async (req, res) => {
     .json({ status: 200, message: "Tweet liked successfully", data: like });
 };
 
-export { toggleVideoLike, toggleTweetLike };
+const toggleCommentLike = async (req, res) => {
+  const { commentId } = req.params;
+
+  // Fetch Comment document and check if Comment exists
+  const comment = await Comment.findById(commentId);
+  if (!comment) {
+    throw new ApiError({ status: 404, message: "Comment does not exist" });
+  }
+
+  // Comment exists and liked by user, so unlike it
+  const unlike = await Like.findOneAndDelete({
+    comment: commentId,
+    likedBy: req.user._id
+  });
+
+  // Response: Comment Unliked
+  if (unlike) {
+    return res.status(200).json(
+      new ApiResponse({
+        status: 200,
+        message: "Comment unliked successfully"
+      })
+    );
+  }
+
+  // Comment exists but not liked by user, so like it
+  const like = await Like.create({
+    comment: commentId,
+    likedBy: req.user._id
+  });
+
+  // Response: Comment Liked
+  return res.status(200).json(
+    new ApiResponse({
+      status: 200,
+      message: "Comment liked successfully",
+      data: like
+    })
+  );
+};
+
+
+
+export { toggleVideoLike, toggleTweetLike, toggleCommentLike };
