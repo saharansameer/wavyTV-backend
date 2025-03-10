a1742d1eb3053123caecf653b4eaf6ea7a944e05
Sameer Saharan <114926999+saharansameer@users.noreply.github.com>
2025-03-10 22:05:11 +0530

    feat: added features to fetch comments of video and tweet

--- src/controllers/comment.controller.js
+++ src/controllers/comment.controller.js
@@ -1,6 +1,7 @@
 import ApiError from "../utils/apiResponse.js";
 import ApiResponse from "../utils/apiResponse.js";
 import { Comment } from "../models/comment.model.js";
+import mongoose from "mongoose";
 
 const addCommentToVideo = async (req, res) => {
   const { videoId } = req.params;
@@ -118,4 +119,109 @@ const deleteComment = async (req, res) => {
     );
 };
 
-export { addCommentToVideo, addCommentToTweet, updateComment, deleteComment };
+const getVideoComments = async (req, res) => {
+  const { videoId } = req.params;
+  const comments = await Comment.aggregate([
+    {
+      $match: {
+        video: mongoose.isValidObjectId(videoId)
+          ? new mongoose.Types.ObjectId(videoId)
+          : null
+      }
+    },
+    {
+      $lookup: {
+        from: "users",
+        localField: "commentBy",
+        foreignField: "_id",
+        as: "commentBy",
+        pipeline: [
+          {
+            $project: {
+              fullName: 1,
+              username: 1,
+              avatar: 1
+            }
+          }
+        ]
+      }
+    },
+    {
+      $addFields: {
+        commentBy: {
+          $first: "$commentBy"
+        }
+      }
+    }
+  ]);
+
+  if (comments.length === 0) {
+    throw new ApiError({ status: 400, message: "Video has no comments" });
+  }
+
+  return res.status(200).json(
+    new ApiResponse({
+      status: 200,
+      message: "Video comments fetched successfully",
+      data: comments
+    })
+  );
+};
+
+const getTweetComments = async (req, res) => {
+  const { tweetId } = req.params;
+  const comments = await Comment.aggregate([
+    {
+      $match: {
+        tweet: mongoose.isValidObjectId(tweetId)
+          ? new mongoose.Types.ObjectId(tweetId)
+          : null
+      }
+    },
+    {
+      $lookup: {
+        from: "users",
+        localField: "commentBy",
+        foreignField: "_id",
+        as: "commentBy",
+        pipeline: [
+          {
+            $project: {
+              fullName: 1,
+              username: 1,
+              avatar: 1
+            }
+          }
+        ]
+      }
+    },
+    {
+      $addFields: {
+        commentBy: {
+          $first: "$commentBy"
+        }
+      }
+    }
+  ]);
+
+  if (comments.length === 0) {
+    throw new ApiError({ status: 400, message: "Tweet has no comments" });
+  }
+
+  return res.status(200).json(
+    new ApiResponse({
+      status: 200,
+      message: "Tweet comments fetched successfully",
+      data: comments
+    })
+  );
+};
+
+export {
+  addCommentToVideo,
+  addCommentToTweet,
+  updateComment,
+  deleteComment,
+  getVideoComments,
+  getTweetComments
+};
