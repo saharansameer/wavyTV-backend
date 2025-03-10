d4f80ed0d0d75c12a08fe20a33f3fe7b2715f4c7
Sameer Saharan <114926999+saharansameer@users.noreply.github.com>
2025-03-10 21:06:34 +0530

    add: added routes for updateComment & deleteComment controllers

--- src/routes/comment.routes.js
+++ src/routes/comment.routes.js
@@ -4,7 +4,9 @@ import { errorHandler } from "../middlewares/error.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 import {
   addCommentToVideo,
-  addCommentToTweet
+  addCommentToTweet,
+  updateComment,
+  deleteComment
 } from "../controllers/comment.controller.js";
 
 const router = Router();
@@ -18,6 +20,13 @@ router
 router
   .route("/tweet/:tweetId")
   .post(asyncHandler(verifyJWT), asyncHandler(addCommentToTweet));
+
+// PATCH - update comment
+// DELETE - delete comment
+router
+  .route("/:commentId")
+  .patch(asyncHandler(verifyJWT), asyncHandler(updateComment))
+  .delete(asyncHandler(verifyJWT), asyncHandler(deleteComment));
 
 // Error Handler
 router.use(errorHandler);
