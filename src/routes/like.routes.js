b6eca48d1652ec4e592e19099213ba3bee941556
Sameer Saharan <114926999+saharansameer@users.noreply.github.com>
2025-03-10 22:42:16 +0530

    add: added routes for toggleCommentLike & getLikedVideos controllers

--- src/routes/like.routes.js
+++ src/routes/like.routes.js
@@ -4,7 +4,9 @@ import { errorHandler } from "../middlewares/error.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 import {
   toggleVideoLike,
-  toggleTweetLike
+  toggleTweetLike,
+  toggleCommentLike,
+  getLikedVideos
 } from "../controllers/like.controller.js";
 
 const router = Router();
@@ -12,11 +14,17 @@ const router = Router();
 // User Authorization
 router.use(asyncHandler(verifyJWT));
 
+// GET - Fetch liked videos (by a user)
+router.route("/video").get(asyncHandler(getLikedVideos));
+
 // POST - Toggle video like
 router.route("/video/:videoId").post(asyncHandler(toggleVideoLike));
 
 // POST - Toggle tweet like
 router.route("/tweet/:tweetId").post(asyncHandler(toggleTweetLike));
+
+// POST - Toggle comment like
+router.route("/comment/:commentId").post(asyncHandler(toggleCommentLike));
 
 // Error Handler
 router.use(errorHandler);
