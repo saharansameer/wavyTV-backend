c1fb3f0a46a4d8327cdb118650ea2bf4c0583f50
Sameer Saharan <114926999+saharansameer@users.noreply.github.com>
2025-03-11 18:33:15 +0530

    add: added routes for getSubscribedChannels controller

--- src/routes/subscription.routes.js
+++ src/routes/subscription.routes.js
@@ -4,7 +4,8 @@ import { errorHandler } from "../middlewares/error.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 import {
   toggleSubscription,
-  getChannelSubscribers
+  getChannelSubscribers,
+  getSubscribedChannels
 } from "../controllers/subscription.controller.js";
 const router = Router();
 
@@ -16,6 +17,9 @@ router.route("/:channelId").post(asyncHandler(toggleSubscription));
 
 // GET - Fetch all subscribers
 router.route("/subscribers").get(asyncHandler(getChannelSubscribers));
+
+// GET - Fetch all subscribed channels
+router.route("/").get(asyncHandler(getSubscribedChannels));
 
 // Error Handler
 router.use(errorHandler);
