ea0d371809de656c486bae151a3cf8d16c039d18
Sameer Saharan <114926999+saharansameer@users.noreply.github.com>
2025-03-11 18:23:40 +0530

    feat: added feature to get a list of subscribers of channel

--- src/controllers/subscription.controller.js
+++ src/controllers/subscription.controller.js
@@ -1,6 +1,7 @@
 import ApiError from "../utils/apiResponse.js";
 import ApiResponse from "../utils/apiResponse.js";
 import { Subscription } from "../models/subscription.model.js";
+import mongoose from "mongoose";
 
 const toggleSubscription = async (req, res) => {
   const { channelId } = req.params;
@@ -35,4 +36,60 @@ const toggleSubscription = async (req, res) => {
   );
 };
 
-export { toggleSubscription };
+const getChannelSubscribers = async (req, res) => {
+  // Fetch all subscribers
+  const subscribers = await Subscription.aggregate([
+    {
+      $match: {
+        channel: mongoose.isValidObjectId(req.user._id)
+          ? new mongoose.Types.ObjectId(req.user._id)
+          : null
+      }
+    },
+    {
+      $lookup: {
+        from: "users",
+        localField: "subscriber",
+        foreignField: "_id",
+        as: "subscriber",
+        pipeline: [
+          {
+            $project: {
+              fullName: 1,
+              username: 1,
+              avatar: 1
+            }
+          }
+        ]
+      }
+    },
+    {
+      $addFields: {
+        subscriber: {
+          $first: "$subscriber"
+        }
+      }
+    },
+    {
+      $project: {
+        subscriber: 1
+      }
+    }
+  ]);
+  
+  // Checks if channel has any subscriber
+  if (subscribers.length === 0) {
+    throw new ApiError({ status: 400, message: "Channel has no subscribers" });
+  }
+
+  // Final Response
+  return res.status(200).json(
+    new ApiResponse({
+      status: 200,
+      message: "Subscribers fetched successfully",
+      data: subscribers
+    })
+  );
+};
+
+export { toggleSubscription, getChannelSubscribers };
